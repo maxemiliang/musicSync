7cbc84ee0ea6ed65b0c8aadb296a8e830b26a11c
maxemiliang <maxemiliang@gmail.com>
2016-03-16 13:31:24 +0200

    Inital Commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'musick'
+});
+
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+  	title: 'SyncMusic', 
+  	slogan: 'We sync your music!'
+  });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { 
+  	title: 'SyncMusic', 
+  	slogan: 'We sync your music!'
+  });
+});
+
+module.exports = router;
