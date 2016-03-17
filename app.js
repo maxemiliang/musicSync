c364e8a0e48d42baf3557ffc010d4f0bfab5e3de
maxemiliang <maxemiliang@gmail.com>
2016-03-17 11:30:20 +0200

    ayy added login! :DD

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -20,6 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'oeffsoidfsgodfjgoejfoeifsjfjsofjeofdobjfborogfojfoefjsfisjfisejfosdijf',
+                 saveUninitialized: false,
+                 resave: false
+                }));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs-plus');
+var password = require('password-hash-and-salt');
+process.setMaxListeners(0);
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'musick'
 });
 
-connection.connect();
+db.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	title: 'SyncMusic', 
-  	slogan: 'We sync your music!'
-  });
+  res.render('index');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { 
-  	title: 'SyncMusic', 
-  	slogan: 'We sync your music!'
-  });
+  res.render('register');
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { 
-  	title: 'SyncMusic', 
-  	slogan: 'We sync your music!'
-  });
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+    db.query('SELECT * FROM users WHERE username = ?', [req.body.username], function(err, results){
+        if (err) throw err;
+
+        if(results.length > 0) {
+            password(req.body.password).verifyAgainst(results[0]['password'], function(err, verified) {
+                if(err) throw new Error('Something happened!');
+                if(verified) {
+                    req.session.userID = results[0]['uID'];
+                    res.redirect('/');
+                } else {
+                    res.redirect('/login'); 
+                }
+            });        
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
+
+router.post('/register', function(req, res, next) {
+	var hashed;
+    if(req.body.username.length > 5 && req.body.password.length > 7) {
+        password(req.body.password).hash(function(err, hash){
+            if (err) throw new Error('Something happened!');
+            hashed = hash;
+            db.query('INSERT INTO users (username, password) VALUES (?, ?)', [req.body.username, hashed], function(err, results){
+                if (err) throw err;
+                
+                res.redirect('/')
+            })
+        });
+    } else {
+        res.redirect('/register/1')
+    }
 });
 
 
