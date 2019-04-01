c06bedc4288d6580eb957515d20fd208537f6b1d
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-04-01 20:47:38 +0300

    Updated so the logs are concise and can be parsed by a log handler

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
